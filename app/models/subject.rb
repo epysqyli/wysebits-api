808445578296921bb75635c37640ce0ffcc94007
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-09-26 15:36:12 +0200

    Add comment to subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,5 @@
 class Subject < ApplicationRecord
   has_and_belongs_to_many :books, join_table: 'subjects_books', foreign_key: 'book_id'
 end
+
+# add model callback to implement automatic double binding association on many-to-many 
