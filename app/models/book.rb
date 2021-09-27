817967577c1bf7dd33b14e5fa6f868932a1ae6ca
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-09-27 21:20:05 +0200

    Add book methods for HABTM relationships

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,14 @@ class Book < ApplicationRecord
   has_many :book_tiles
   has_and_belongs_to_many :authors, join_table: 'authors_books', foreign_key: 'author_id'
   has_and_belongs_to_many :subjects, join_table: 'subjects_books', foreign_key: 'subject_id'
+
+  def add_subject(subject)
+    subjects << subject
+    subject.books << self
+  end
+
+  def add_author(author)
+    authors << author
+    author.books << self
+  end
 end
