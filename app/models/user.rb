4574871ff9ed51d3f0d4d5e1b472b69b966dab5b
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-09-27 20:03:45 +0200

    Fix user passive_relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
 
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
-  has_many :followers, through: :active_relationships
+  has_many :followers, through: :passive_relationships
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :email_address, presence: true, length: { minimum: 4, maximum: 125 },
--- db/schema.rb
+++ db/schema.rb
@@ -10,104 +10,103 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_163656) do
-
+ActiveRecord::Schema.define(version: 20_210_927_163_656) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "authors", force: :cascade do |t|
-    t.string "name"
-    t.string "surname"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'authors', force: :cascade do |t|
+    t.string 'name'
+    t.string 'surname'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "authors_books", id: false, force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "book_id"
-    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id"
-    t.index ["author_id"], name: "index_authors_books_on_author_id"
-    t.index ["book_id"], name: "index_authors_books_on_book_id"
+  create_table 'authors_books', id: false, force: :cascade do |t|
+    t.bigint 'author_id'
+    t.bigint 'book_id'
+    t.index %w[author_id book_id], name: 'index_authors_books_on_author_id_and_book_id'
+    t.index ['author_id'], name: 'index_authors_books_on_author_id'
+    t.index ['book_id'], name: 'index_authors_books_on_book_id'
   end
 
-  create_table "book_tiles", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id"], name: "index_book_tiles_on_book_id"
-    t.index ["user_id"], name: "index_book_tiles_on_user_id"
+  create_table 'book_tiles', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'book_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['book_id'], name: 'index_book_tiles_on_book_id'
+    t.index ['user_id'], name: 'index_book_tiles_on_user_id'
   end
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.date "release_date"
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_books_on_category_id"
+  create_table 'books', force: :cascade do |t|
+    t.string 'title'
+    t.date 'release_date'
+    t.bigint 'category_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_books_on_category_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "commentable_id"
-    t.string "commentable_type"
-    t.bigint "user_id"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'comments', force: :cascade do |t|
+    t.bigint 'commentable_id'
+    t.string 'commentable_type'
+    t.bigint 'user_id'
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  create_table 'relationships', force: :cascade do |t|
+    t.integer 'follower_id'
+    t.integer 'followed_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['followed_id'], name: 'index_relationships_on_followed_id'
+    t.index %w[follower_id followed_id], name: 'index_relationships_on_follower_id_and_followed_id', unique: true
+    t.index ['follower_id'], name: 'index_relationships_on_follower_id'
   end
 
-  create_table "subjects", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'subjects', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "subjects_books", id: false, force: :cascade do |t|
-    t.bigint "subject_id"
-    t.bigint "book_id"
-    t.index ["book_id"], name: "index_subjects_books_on_book_id"
-    t.index ["subject_id", "book_id"], name: "index_subjects_books_on_subject_id_and_book_id"
-    t.index ["subject_id"], name: "index_subjects_books_on_subject_id"
+  create_table 'subjects_books', id: false, force: :cascade do |t|
+    t.bigint 'subject_id'
+    t.bigint 'book_id'
+    t.index ['book_id'], name: 'index_subjects_books_on_book_id'
+    t.index %w[subject_id book_id], name: 'index_subjects_books_on_subject_id_and_book_id'
+    t.index ['subject_id'], name: 'index_subjects_books_on_subject_id'
   end
 
-  create_table "tile_entries", force: :cascade do |t|
-    t.text "content"
-    t.integer "upvotes"
-    t.integer "downvotes"
-    t.bigint "book_tile_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_tile_id"], name: "index_tile_entries_on_book_tile_id"
+  create_table 'tile_entries', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'upvotes'
+    t.integer 'downvotes'
+    t.bigint 'book_tile_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['book_tile_id'], name: 'index_tile_entries_on_book_tile_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "surname"
-    t.string "email_address"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'surname'
+    t.string 'email_address'
+    t.string 'password_digest'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "book_tiles", "books"
-  add_foreign_key "book_tiles", "users"
-  add_foreign_key "books", "categories"
-  add_foreign_key "tile_entries", "book_tiles"
+  add_foreign_key 'book_tiles', 'books'
+  add_foreign_key 'book_tiles', 'users'
+  add_foreign_key 'books', 'categories'
+  add_foreign_key 'tile_entries', 'book_tiles'
 end
