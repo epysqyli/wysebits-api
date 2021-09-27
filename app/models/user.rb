687308020aa699ad38c32425accd2cb17994f267
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-09-27 18:56:48 +0200

    Add Relationship model, setup user follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   has_many :book_tiles
   has_many :comments
 
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :active_relationships
+
   validates :name, presence: true, length: { minimum: 3 }
   validates :email_address, presence: true, length: { minimum: 4, maximum: 125 },
                             format: { with: /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/, multiline: true, message: 'Invalid format' }
---
+++ db/migrate/20210927163538_drop_relationships_table.rb
@@ -0,0 +1,5 @@
+class DropRelationshipsTable < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :relationships
+  end
+end
