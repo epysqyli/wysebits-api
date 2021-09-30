34ddfb90be3c99290b638ec6854848a23af9665f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-09-30 21:38:38 +0200

    Add index show actions and private methods to TileEntriesController

--- app/controllers/tile_entries_controller.rb
+++ app/controllers/tile_entries_controller.rb
@@ -1,13 +1,17 @@
 class TileEntriesController < ApplicationController
   before_action :book_tile, only: %i[index create]
+  before_action :tile_entry, only: :show
   skip_before_action :authenticate_request, only: %i[index show]
 
   def index
     @tile_entries = book_tile.tile_entries
+    # use uniform 'data' key instead of tile_entries
     render json: { tile_entries: @tile_entries }
   end
 
-  def show; end
+  def show
+    render json: { data: tile_entry } if tile_entry
+  end
 
   def create; end
 
@@ -16,6 +20,10 @@ class TileEntriesController < ApplicationController
   def destroy; end
 
   private
+
+  def tile_entry
+    TileEntry.find(params[:id])
+  end
 
   def book_tile
     BookTile.find(params[:book_tile_id])
