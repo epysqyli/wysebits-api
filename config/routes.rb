4fabdc37a4791087a98b30e7f3ef4033bbdc22e9
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-09-30 20:36:18 +0200

    Add comment to routes for further steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
 
+  get '/all_book_tiles', to: 'book_tiles#tiles_index'
+
+  # limit routes accordingly with only: :actions
   resources :users do
     resources :book_tiles
   end
-
-  get '/all_book_tiles', to: 'book_tiles#tiles_index'
 
   resources :book_tiles do
     resources :tile_entries
