6f5af485add486e3ad4b9ca418db017203cdc064
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-23 15:58:24 +0200

    Add set_cookie to authenticate controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,6 +5,15 @@ class AuthenticationController < ApplicationController
     command = AuthenticateUser.call(params[:email_address], params[:password])
 
     if command.success?
+      response.set_cookie(
+        :jwt, {
+          value: command.result,
+          expires: 7.days.from_now,
+          path: '/',
+          httponly: true
+        }
+      )
+
       render json: { auth_token: command.result }
     else
       render json: { error: command.errors }, status: :unauthorized
