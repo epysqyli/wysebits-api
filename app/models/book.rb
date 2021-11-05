191f0df20923d85c0fc7b5bd9e507fab6643c8e4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-05 22:26:10 +0100

    Add callback methods on Book model to keep db in sync with es

--- app/models/book.rb
+++ app/models/book.rb
@@ -28,6 +28,18 @@ class Book < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
+  after_commit on: [:create] do
+    __elasticsearch__.index_document
+  end
+
+  after_commit on: [:update] do
+    __elasticsearch__.update_document
+  end
+
+  after_commit on: [:destroy] do
+    __elasticsearch__.delete_document
+  end
+
   settings index: { number_of_shards: 2 } do
     mapping dynamic: false do
       indexes :title, analyzer: :english
