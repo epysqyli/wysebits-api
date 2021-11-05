55b990fc4098abdc98dbbbb66fcf7829094ddc6b
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-05 19:12:29 +0100

    Add id to categories controller index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_request, only: :index
 
   def index
-    @categories = Category.select(:name, :slug)
+    @categories = Category.select(:name, :slug, :id)
     render json: { data: @categories }
   end
 end
