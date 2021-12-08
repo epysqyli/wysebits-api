aaadd81cdf17b925f76f07fb200599f7cb5567c3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-08 10:45:18 +0100

    Add pagination to search_requests controller

--- app/controllers/search_requests_controller.rb
+++ app/controllers/search_requests_controller.rb
@@ -1,22 +1,22 @@
 class SearchRequestsController < ApplicationController
-  include Pagy::Backend
   skip_before_action :authenticate_request, only: :search_books
 
   def search_books
     search_terms = JSON.parse(search_params[:keywords])
-    # from = JSON.parse(search_params[:from])
-    results = Book.search(search_terms)
+    page_num = JSON.parse(search_params[:page_num])
+    from = page_num == 1 ? 0 : page_num * 20
+    results = Book.search(search_terms, from)
 
     if results.empty?
       render json: { message: 'No results. Do you want to create this book record?' }
     else
-      render json: results
+      render json: { results: results, page_num: page_num }
     end
   end
 
   private
 
   def search_params
-    params.permit(:keywords, :from, search_request: [:keywords])
+    params.permit(:keywords, :page_num, search_request: [:keywords])
   end
 end
