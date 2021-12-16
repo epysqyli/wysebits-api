025561aadf10c63672bcafb2fa2ecde18b1c10d7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-16 12:21:41 +0100

    Organize routes file with comments to identify sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 Rails.application.routes.draw do
   scope path: '/api' do
+    # auth
     post '/login', to: 'authentication#authenticate'
     get '/logout', to: 'authentication#logout'
     get '/logged_in', to: 'authentication#logged_in'
     post '/signup', to: 'users#create'
 
+    # user relationships
     get '/users/:id/following', to: 'users#following'
     get '/users/:id/followers', to: 'users#followers'
-
     post '/users/:id/follow', to: 'users#add_following'
     post '/users/:id/unfollow', to: 'users#remove_following'
 
+    # user fav books
     get '/users/:id/fav_books', to: 'users#fav_books'
     post '/users/:id/fav_books/:book_id', to: 'users#add_to_fav_books'
     delete '/users/:id/fav_books/:book_id', to: 'users#remove_from_fav_books'
 
+    # user fav entries
     get '/users/:id/fav_tile_entries', to: 'users#fav_tile_entries'
     post '/users/:id/fav_tile_entries/:tile_entry_id', to: 'users#add_to_fav_tile_entries'
     delete '/users/:id/fav_tile_entries/:tile_entry_id', to: 'users#remove_from_fav_tile_entries'
 
+    # user upvoting / downvoting entries
     get '/users/:id/upvoted_entries', to: 'users#upvoted_entries'
     get '/users/:id/downvoted_entries', to: 'users#downvoted_entries'
     post '/users/:id/tile_entries/:tile_entry_id/upvote', to: 'users#upvote'
@@ -26,9 +30,11 @@ Rails.application.routes.draw do
     post '/users/:id/tile_entries/:tile_entry_id/downvote', to: 'users#downvote'
     post '/users/:id/tile_entries/:tile_entry_id/remove_downvote', to: 'users#remove_downvote'
 
+    # top tiles? to be checked
     get '/top_tiles', to: 'tile_entries#top_tiles'
     get '/all_tiles_from_book/:id', to: 'books#tiles'
 
+    # resources
     resources :categories, only: :index
     get '/categories/:slug/books', to: 'categories#books'
 
