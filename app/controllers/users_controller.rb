4c5cc08318a19a7e2e6b37d38d34e854493f6fb1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-16 17:06:07 +0100

    Add necessary associated models to users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,10 @@ class UsersController < ApplicationController
 
   def show
     if user
-      book_tiles = user.book_tiles.order(created_at: :desc).as_json(include: %i[book tile_entries])
+      book_tiles = user.book_tiles.order(created_at: :desc)
+                       .as_json(include:
+                        [:tile_entries, { book: { include: %i[authors category] } }])
+
       render json: { username: user.username, book_tiles: book_tiles }
     else
       render json: { message: 'User not found' }, status: 404
