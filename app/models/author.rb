214246bbfcfb1cafd2d520cd6ec0f339b1cc22fe
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-11 22:05:10 +0100

    Setup Author model concern AuthorSearchable

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
+  #elasticsearch concern
+  include AuthorSearchable
+
   has_and_belongs_to_many :books, join_table: 'authors_books', foreign_key: 'author_id'
 
   # model methods
--- app/models/concerns/author_searchable.rb
+++ app/models/concerns/author_searchable.rb
@@ -2,6 +2,48 @@ module AuthorSearchable
   extend ActiveSupport::Concern
 
   included do
-    
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
+
+    def self.bulk_index(authors)
+      __elasticsearch__.client.bulk({
+                                      index: __elasticsearch__.index_name,
+                                      type: '_doc',
+                                      body: Parallel.map(authors, in_threads: 4) do |author|
+                                              { index: { _id: author.id, data: author } }
+                                            end
+                                    })
+    end
+
+    def self.import
+      in_batches { |authors| bulk_index(authors) }
+    end
+
+    mapping dynamic: false do
+      indexes :full_name, analyzer: :standard
+    end
+
+    def self.search(query, from = 0)
+      __elasticsearch__.search(
+        {
+          query: {
+            match:
+            {
+              full_name: {
+                query: query,
+                fuzziness: 'AUTO'
+              }
+            }
+          },
+          size: 20,
+          from: from,
+          highlight: {
+            pre_tags: ['<b>'],
+            post_tags: ['</b>'],
+            fields: { full_name: {} }
+          }
+        }
+      )
+    end
   end
 end
