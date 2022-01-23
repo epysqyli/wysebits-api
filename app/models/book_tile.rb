168e2b6d0d9fd14fe6df1650d29ce0801052a321
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-23 22:03:20 +0100

    Add after_commit callback on BookTile

--- app/models/book_tile.rb
+++ app/models/book_tile.rb
@@ -3,4 +3,12 @@ class BookTile < ApplicationRecord
   belongs_to :book
   has_many :tile_entries, dependent: :destroy
   has_many :temporary_entries, dependent: :destroy
+
+  def update_book_tiles_count
+    book = Book.find book_id
+    book.tiles_count += 1
+    book.save
+  end
+
+  after_commit :update_book_tiles_count
 end
