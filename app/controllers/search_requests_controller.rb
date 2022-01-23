befda6e3d074843adffea308b1c90f6824a22baa
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-23 18:46:19 +0100

    Rewrite search_requests_controller for pg search

--- app/controllers/search_requests_controller.rb
+++ app/controllers/search_requests_controller.rb
@@ -1,31 +1,18 @@
 class SearchRequestsController < ApplicationController
+  include Pagy::Backend
+
   before_action :search_params
   skip_before_action :authenticate_request
 
   def search_books
-    search_terms = JSON.parse(search_params[:keywords])
-    page_num = JSON.parse(search_params[:page_num])
-    from = page_num == 1 ? 0 : page_num * 20
-    results = Book.search(search_terms, from)
-
-    if results.empty?
-      render json: { message: 'No results. Do you want to create this book record?' }
-    else
-      render json: { results: results, page_num: page_num }
-    end
+    pagy, books = pagy(Book.search_book(search_params[:keywords]))
+    resp = books.includes(:authors, :category).as_json(include: %i[authors category])
+    render json: { results: resp, pagy: pagy_metadata(pagy) }
   end
 
   def search_authors
-    search_terms = JSON.parse(search_params[:keywords])
-    page_num = JSON.parse(search_params[:page_num])
-    from = page_num == 1 ? 0 : page_num * 20
-    results = Author.search(search_terms, from)
-
-    if results.empty?
-      render json: { message: 'No results' }
-    else
-      render json: { results: results, page_num: page_num }
-    end
+    pagy, authors = pagy(Author.search_author(search_params[:keywords]))
+    render json: { results: authors, pagy: pagy_metadata(pagy) }
   end
 
   private
