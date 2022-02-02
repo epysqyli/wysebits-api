97164707d0d215a62dc302c05df3efd19dfa82c5
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-02-02 15:59:16 +0100

    Add before action for user stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,14 @@
 class StatsController < ApplicationController
+  before_action :user, only: :user_stats
   skip_before_action :authenticate_request
 
   def user_stats; end
 
   def trending; end
+
+  private
+
+  def user
+    User.find params[:id]
+  end
 end
