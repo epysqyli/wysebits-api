40359ab0c96dc9eb2a7b0cbc731444c4a3142653
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-02-02 11:57:55 +0100

    Add avatar_url to users#following/followers json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -146,7 +146,7 @@ class UsersController < ApplicationController
       .includes({ followed: [{ book_tiles: :tile_entries }] }))
 
     resp = user_following.as_json(include:
-      { followed: { only: %i[username id], include: { book_tiles: { include: :tile_entries } } } })
+      { followed: { only: %i[username id avatar_url], include: { book_tiles: { include: :tile_entries } } } })
     render json: { following: resp, pagy: pagy_metadata(pagy) }
   end
 
@@ -159,7 +159,7 @@ class UsersController < ApplicationController
       .includes({ follower: [{ book_tiles: :tile_entries }] }))
 
     resp = user_followers.as_json(include:
-      { follower: { only: %i[username id], include: { book_tiles: { include: :tile_entries } } } })
+      { follower: { only: %i[username id avatar_url], include: { book_tiles: { include: :tile_entries } } } })
     render json: { followers: resp, pagy: pagy_metadata(pagy) }
   end
 
