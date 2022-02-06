bc41e55444e00fa7c33c36356312bfc232846889
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-02-06 13:39:27 +0100

    add conversions#create

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,7 +3,14 @@ class ConversationsController < ApplicationController
 
   def index; end
 
-  def create; end
+  def create
+    if Conversation.valid?(conversation_params[:sender_id], conversation_params[:recipient_id])
+      Conversation.create! sender_id: conversation_params[:sender_id], recipient_id: conversation_params[:recipient_id]
+      render json: { status: 'ok' }, status: :ok
+    else
+      render json: { status: 'Conversation not created' }, status: :forbidden
+    end
+  end
 
   private
 
