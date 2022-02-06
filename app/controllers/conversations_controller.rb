7e21cbf55a5d0fcee4e1183393fcf81507889151
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-02-06 14:00:15 +0100

    Add conversations#index

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,11 @@
 class ConversationsController < ApplicationController
   before_action :conversation_params, only: :create
+  before_action :user, only: :index
 
-  def index; end
+  def index
+    conversations = user.conversations.includes(:messages).order(created_at: :desc)
+    render json: { conversations: conversations.as_json(include: :messages) }
+  end
 
   def create
     if Conversation.valid?(conversation_params[:sender_id], conversation_params[:recipient_id])
@@ -13,6 +17,10 @@ class ConversationsController < ApplicationController
   end
 
   private
+
+  def user
+    User.find params[:user_id]
+  end
 
   def conversation_params
     params.permit(:sender_id, :recipient_id)
