8d7c8acdb3e5357c85255d6aef614b30707eed16
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-06 16:21:57 +0100

    email: improve signup_confirmation email

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,10 +1,14 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview all emails at http://localhost:3001/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def signup_confirmation
     UserMailer.with(user: User.last).signup_confirmation
   end
 
   def reset_password
-    UserMailer.with(user: User.last).signup_confirmation
+    UserMailer.with(user: User.last).reset_password
+  end
+
+  def update_email
+    UserMailer.with(user: User.last).update_email
   end
 end
