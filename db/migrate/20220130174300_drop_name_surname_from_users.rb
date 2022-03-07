914c3a9c864500e7b5819ad1c24ff64ade98996c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-07 20:56:17 +0100

    migrations: add missing type to drop_name_surname_from_users

--- db/migrate/20220130174300_drop_name_surname_from_users.rb
+++ db/migrate/20220130174300_drop_name_surname_from_users.rb
@@ -1,6 +1,6 @@
 class DropNameSurnameFromUsers < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :name
-    remove_column :users, :surname
+    remove_column :users, :name, :string
+    remove_column :users, :surname, :string
   end
 end
