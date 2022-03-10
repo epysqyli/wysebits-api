1ebc9b93a23015e7760f5a3b36ddac3c8b5362ad
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-10 21:41:49 +0100

    weekly trending: save each transaction in job

--- app/jobs/week_trend_job.rb
+++ app/jobs/week_trend_job.rb
@@ -13,6 +13,7 @@ class WeekTrendJob < ApplicationJob
     books = BookTile.includes(:book).map(&:book)
     books.each do |book|
       book.tiles_count_diff = book.tiles_count - book.previous_tiles_count
+      book.save
     end
   end
 
@@ -20,12 +21,14 @@ class WeekTrendJob < ApplicationJob
     users = BookTile.includes(:user).map(&:user)
     users.each do |user|
       user.tiles_count_diff = user.tiles_count - user.previous_tiles_count
+      user.save
     end
   end
 
   def process_trending_insight
     TileEntry.all.each do |insight|
       insight.upvotes_diff = insight.upvotes - insight.previous_upvotes
+      insight.save
     end
   end
 end
