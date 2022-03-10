08335148946059dc3d14429c7c0d1b2db80a172a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-10 22:32:24 +0100

    weekly trend controller: rearrange results

--- app/controllers/week_trend_controller.rb
+++ app/controllers/week_trend_controller.rb
@@ -2,24 +2,21 @@ class WeekTrendController < ApplicationController
   skip_before_action :authenticate_request
 
   def index
-    render json: { book: trending_book, user: json_user, insight: trending_insight }, status: :ok
+    render json: { book: trending_book, user: trending_user, insight: trending_insight }, status: :ok
   end
 
   private
 
   def trending_book
-    Book.order(tiles_count_diff: :desc).first
+    Book.order(tiles_count_diff: :desc).first.as_json(include: %i[authors category])
   end
 
   def trending_user
-    User.order(tiles_count_diff: :desc).first
-  end
-
-  def json_user
-    trending_user.as_json(only: %i[username tiles_count_diff avatar_url])
+    User.order(tiles_count_diff: :desc).first.as_json(only: %i[username tiles_count_diff avatar_url])
   end
 
   def trending_insight
     TileEntry.order(upvotes_diff: :desc).first
+             .as_json(include: { book_tile: { include: [:book, { user: { only: %i[id username] } }] } })
   end
 end
