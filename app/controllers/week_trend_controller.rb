40cb359b2487200333daad9a9c91ec114e2b5404
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-10 21:48:55 +0100

    weekly trend: render only necessary user fields

--- app/controllers/week_trend_controller.rb
+++ app/controllers/week_trend_controller.rb
@@ -2,7 +2,7 @@ class WeekTrendController < ApplicationController
   skip_before_action :authenticate_request
 
   def index
-    render json: { book: trending_book, user: trending_user, insight: trending_insight }, status: :ok
+    render json: { book: trending_book, user: json_user, insight: trending_insight }, status: :ok
   end
 
   private
@@ -13,6 +13,10 @@ class WeekTrendController < ApplicationController
 
   def trending_user
     User.order(tiles_count_diff: :desc).first
+  end
+
+  def json_user
+    trending_user.as_json(only: %i[username tiles_count_diff avatar_url])
   end
 
   def trending_insight
