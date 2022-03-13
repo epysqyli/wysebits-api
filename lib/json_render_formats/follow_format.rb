210d52c89e4de5db798296f5cb0984286a73cf41
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-13 17:54:16 +0100

    follow format: fix included relationships

--- lib/json_render_formats/follow_format.rb
+++ lib/json_render_formats/follow_format.rb
@@ -1,6 +1,11 @@
 class FollowFormat
-  def self.json_follow_booktile_entries(input)
+  def self.json_follow_booktile_entries_follower(input)
     input.as_json(include:
       { follower: { only: %i[username id avatar_url], include: { book_tiles: { include: :tile_entries } } } })
+  end
+
+  def self.json_follow_booktile_entries_followed(input)
+    input.as_json(include:
+      { followed: { only: %i[username id avatar_url], include: { book_tiles: { include: :tile_entries } } } })
   end
 end
