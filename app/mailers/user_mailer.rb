3e3287b711508660909b1c7b7f4cb0cdd3d8afaa
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-03 18:15:42 +0200

    config: set base_url env

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,18 +3,21 @@ class UserMailer < ApplicationMailer
 
   def signup_confirmation
     @user = params[:user]
+    @base_url = ENV['base_url']
     attachments.inline['logo'] = File.read(Rails.root.join('public/gimp-logo-black.png'))
     mail(to: @user.email_address, subject: 'Activate your wysebits account')
   end
 
   def reset_password
     @user = params[:user]
+    @base_url = ENV['base_url']
     attachments.inline['logo'] = File.read(Rails.root.join('public/gimp-logo-black.png'))
     mail(to: @user.email_address, subject: 'Reset password for your Wysebits account')
   end
 
   def update_email
     @user = params[:user]
+    @base_url = ENV['base_url']
     attachments.inline['logo'] = File.read(Rails.root.join('public/gimp-logo-black.png'))
     mail(to: @user.unconfirmed_email, subject: 'Change your email address for Wysebits')
   end
