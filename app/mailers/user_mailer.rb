6fbe35fc9d18aa1063bb3496fc8632db4c7f385d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-03 16:20:28 +0200

    application mailer: add text/html content type

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,19 +2,16 @@ class UserMailer < ApplicationMailer
   default from: 'wysebits@gmail.com'
 
   def signup_confirmation
-    content_type 'text/html'
     @user = params[:user]
-    mail(to: @user.email_address, subject: 'Activate your wysebits account')
+    mail(to: @user.email_address, subject: 'Activate your wysebits account', content_type: 'text/html')
   end
 
   def reset_password
-    content_type 'text/html'
     @user = params[:user]
     mail(to: @user.email_address, subject: 'Reset password for your Wysebits account')
   end
 
   def update_email
-    content_type 'text/html'
     @user = params[:user]
     mail(to: @user.unconfirmed_email, subject: 'Change your email address for Wysebits')
   end
