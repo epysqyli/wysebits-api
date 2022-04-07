7514edc4723f444459b93c45d50d85cc88fdb676
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-07 22:39:36 +0200

    user_format: fix json format

--- lib/json_render_formats/user_format.rb
+++ lib/json_render_formats/user_format.rb
@@ -1,5 +1,5 @@
 class UserFormat
   def self.username_tiles_diff_avatar(input)
-    input.as_json(include: { only: %i[username tiles_count_diff avatar_url] })
+    input.as_json(only: %i[username tiles_count_diff avatar_url])
   end
 end
