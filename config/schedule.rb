71f5648bd978fc28af00eddb01f0545a9ea515e3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-17 21:18:20 +0200

    crontab: add daily recommendations update via whenever schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ env :PATH, ENV['PATH']
 set :output, 'log/cron.log'
 set :environment, 'production'
 
+every 1.day do
+  runner 'RecommendedBooksJob.perform_later'
+end
+
 every 7.days do
   runner 'WeekTrendJob.perform_later'
 end
