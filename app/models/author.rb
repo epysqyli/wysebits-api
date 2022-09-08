600fe1a8ef34494c96f36dc73cad12bacec4e39c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-09-08 22:10:48 +0200

    authors: remove uniqueness of full_name check.

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,5 +6,5 @@ class Author < ApplicationRecord
 
   has_and_belongs_to_many :books, join_table: 'authors_books', foreign_key: 'author_id'
 
-  validates_uniqueness_of :full_name, on: :create
+  # validates_uniqueness_of :full_name, on: :create
 end
