e827a2812bceb46160c1daa78c4b245ce09d80bc
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-09-18 16:03:35 +0200

    elastic: add build_query to ElasticQuery.

--- app/controllers/elastic_search_controller.rb
+++ app/controllers/elastic_search_controller.rb
@@ -3,14 +3,8 @@ class ElasticSearchController < ApplicationController
 
   def search_books
     elastic_query = ElasticQuery.new
-    elastic_query.add_match_to_must 'title', search_params[:search_term]
+    elastic_query.build_query(params[:search_query])
 
     render json: Book.search(elastic_query)
-  end
-
-  private
-
-  def search_params
-    params.permit(:search_term)
   end
 end
--- lib/elastic_query.rb
+++ lib/elastic_query.rb
@@ -10,20 +10,21 @@ class ElasticQuery
     }
   end
 
-  def add_match_to_must(field, value, fuzziness = 'AUTO')
+  def add_match_block_to_must(field, value, fuzziness = 'AUTO')
     @query[:bool][:must] << match_block(field, value, fuzziness)
   end
 
-  def add_match_to_filter(field, value, fuzziness = 'AUTO')
+  def add_match_block_to_filter(field, value, fuzziness = 'AUTO')
     @query[:bool][:filter] << match_block(field, value, fuzziness)
-  end
-
-  def add_term_block_to_must(field, value)
-    @query[:bool][:must] << term_block(field, value)
   end
 
   def add_term_block_to_filter(field, value)
     @query[:bool][:filter] << term_block(field, value)
+  end
+
+  def build_query(search_request)
+    build_match_query(search_request)
+    build_term_query(search_request)
   end
 
   private
@@ -45,5 +46,21 @@ class ElasticQuery
         field => value
       }
     }
+  end
+
+  def build_match_query(search_request)
+    match_query = search_request[:match].presence
+
+    must_match_query = match_query[:must].presence if match_query
+    must_match_query.first.each_pair { |k, v| add_match_block_to_must(k, v) } if must_match_query.present?
+
+    filter_match_query = match_query[:filter].presence if match_query
+    filter_match_query.first.each_pair { |k, v| add_match_block_to_filter(k, v) } if filter_match_query.present?
+  end
+
+  def build_term_query(search_request)
+    term_query = search_request[:term].presence
+    filter_term_query = term_query[:filter].presence if term_query
+    filter_term_query.first.each_pair { |k, v| add_term_block_to_filter(k, v) } if filter_term_query.present?
   end
 end
