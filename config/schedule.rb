3b65b0e0b671eee35c58692cd48061339f59808a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-10-06 00:06:14 +0200

    whenever: add rake tasks.

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,3 +9,8 @@ end
 every 7.days do
   runner 'WeekTrendJob.perform_later'
 end
+
+every '0 0 6 * *' do
+  rake "db:import_books['/home/elvis/tasks/openlibrary-diff-go/recent_works.txt']"
+  rake "db:import_books['/home/elvis/tasks/openlibrary-diff-go/recent_authors.txt']"
+end
